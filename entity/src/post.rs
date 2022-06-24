429b17c3c668a1d0cca0f23a6a40d633888398ab
XiaoQu <lqqu@thoughtworks.com>
2022-06-24 16:14:16 +0800

    fix: fix id not found error

--- entity/src/post.rs
+++ entity/src/post.rs
@@ -5,6 +5,7 @@ use serde::{Deserialize, Serialize};
 #[sea_orm(table_name = "posts")]
 pub struct Model {
     #[sea_orm(primary_key)]
+    #[serde(skip_deserializing)]
     pub id: i32,
     pub title: String,
     #[sea_orm(column_type = "Text")]
